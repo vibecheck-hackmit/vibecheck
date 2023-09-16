bd23bceb008e572de67824d024a8cba5017701d8
Firangiz Ganbarli <65179277+firangizg@users.noreply.github.com>
2023-09-16 15:17:35 -0400

    track object

--- spotify.py
+++ spotify.py
@@ -1,34 +1,34 @@
 import json
 import spotipy
-import numpy as np
 from spotipy.oauth2 import SpotifyOAuth
 
+class Track: 
+    def __init__(self, track_name, artist_name, danceability, energy, valence, key):
+        self.track_name = track_name
+        self.artist_name = artist_name
+        self.danceability = danceability
+        self.energy = energy
+        self.valence = valence
+        self.key = key
+
+    def __repr__(self):
+        return f"Track(name={self.track_name}, artist={self.artist_name}, danceability={self.danceability}, energy={self.energy}, valence={self.valence}, key={self.key})"
+
 def get_top_tracks_features():
-    # Load client ID and secret from .creds/spotify.json file
     with open('.creds/spotify.json', 'r') as f:
         creds = json.load(f)
-        CLIENT_ID = creds['CLIENT_ID']
-        CLIENT_SECRET = creds['CLIENT_SECRET']
-
-    # Set up authentication
+        
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
+        client_id=creds['CLIENT_ID'],
+        client_secret=creds['CLIENT_SECRET'],
         redirect_uri="http://localhost:8888/callback", 
         scope=["user-top-read"]
     ))
 
-    # Fetching top 10 tracks from the last month
-    time_range = 'short_term'  
-    results = sp.current_user_top_tracks(limit=10, time_range=time_range)
+    results = sp.current_user_top_tracks(limit=10, time_range='short_term')
+    audio_features = sp.audio_features([track['id'] for track in results['items']])
 
-    # Get track IDs
-    track_ids = [track['id'] for track in results['items']] 
-
-    # Fetch audio features for these tracks
-    audio_features = sp.audio_features(track_ids)
-
-    # Create a list of tuples with the results
+    # Create a list of objects
     tracks_info = []
     for track, feature in zip(results['items'], audio_features):
         track_name = track['name']
@@ -36,11 +36,15 @@ def get_top_tracks_features():
         danceability = feature['danceability']
         energy = feature['energy']
         valence = feature['valence']
+        key = feature['key']
         
-        tracks_info.append([danceability, energy, valence])
+        track_obj = Track(track_name, artist_name, danceability, energy, valence, key)
+        tracks_info.append(track_obj)
 
-    # Close the Spotify session
-    sp = None
+    return tracks_info
 
-    # Convert to numpy array and return
-    return np.array(tracks_info, dtype=object)
+# Test
+if __name__ == "__main__":
+    tracks = get_top_tracks_features()
+    for track in tracks:
+        print(track)
