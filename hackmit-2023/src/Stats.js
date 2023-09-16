b3d7ca3b5b72648b20f1fe82640ae35d3af451c9
zoekulphong <zckulphong@gmail.com>
2023-09-16 18:58:15 -0400

    Final frontend stuff

--- hackmit-2023/src/Stats.js
+++ hackmit-2023/src/Stats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 import './App.css';
 
 import List from './List.js';
@@ -7,14 +8,16 @@ import data from './sample.json';
 
 function Stats() {
   return (
-    <div>
+    <Fade cascade>
       <h2>Statistics</h2>
       <div className="stats-container">
-        <List data={data} title="Top Tracks" category="track_name"/>
-        <List data={data} title="Top Artists" category="artist_name"/>
-        <BarChart data={data}/>
+        <Fade>
+          <List data={data} title="Top Tracks" category="track_name"/>
+          <List data={data} title="Top Artists" category="artist_name"/>
+          <BarChart data={data}/>
+        </Fade>
       </div>
-    </div>
+    </Fade>
   )
 }
 
