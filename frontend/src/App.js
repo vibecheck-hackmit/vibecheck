01cc8621a0ef4521eb7572ba9f627693456bbfdd
zoekulphong <zckulphong@gmail.com>
2023-09-17 03:31:10 -0400

    i think its working?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,27 +10,39 @@ const CategoryContext = React.createContext({
   category: 0, fetchCategory: () => {}
 })
 
-function App() {
-  const [category, setCategory] = useState([])
-  const fetchCategory = async () => {
-    const response = await fetch("http://localhost:8000/mood-message/")
-    const category = await response.json()
-    setCategory(category)
-  }
-  useEffect(() => {
-    fetchCategory()
-  }, [])
+let cat = 0;
 
-  console.log(category);
+async function test() {
+  let response = await fetch("http://localhost:8000/mood-message/");
+  let category = await response.json();
+  cat = category.closest_match;
+  console.log(category.closest_match);
+}
+
+await test();
+
+function App() {
+  // const [category, setCategory] = useState([])
+  // const fetchCategory = async () => {
+  //   const response = await fetch("http://localhost:8000/mood-message/")
+  //   const category = await response.json()
+  //   setCategory(category)
+  // }
+  // useEffect(() => {
+  //   fetchCategory()
+  // }, [])
+  
+  // console.log(category);
+
   // const vibeMessage = messages.find(item => item.id === 2).message;
   // const vibeMood = messages.find(item => item.id === 2).mood;
   // const vibeColor = messages.find(item => item.id === 2).color;
-  const vibeMessage = messages.find(item => item.id === category.closest_match).message;
-  const vibeMood = messages.find(item => item.id === category.closest_match).mood;
-  const vibeColor = messages.find(item => item.id === category.closest_match).color;
+  const vibeMessage = messages.find(item => item.id === cat).message;
+  const vibeMood = messages.find(item => item.id === cat).mood;
+  const vibeColor = messages.find(item => item.id === cat).color;
 
   return (
-    <CategoryContext.Provider value={{category, fetchCategory}}>
+    // <CategoryContext.Provider value={{category, fetchCategory}}>
       <div className="site-container" style={{backgroundColor: vibeColor}}>
         <Nav />
         <div className="content-container">
@@ -38,7 +50,7 @@ function App() {
           <Stats />
         </div>
       </div>
-    </CategoryContext.Provider>
+    // </CategoryContext.Provider>
   );
 }
 
--- frontend/src/Message.js
+++ frontend/src/Message.js
@@ -6,6 +6,7 @@ function Message(props) {
   return (
     <Fade big cascade>
       <div className="message-container" id="message">
+        <h2>Vibe: {props.mood}</h2>
         <h1>{props.message}</h1>
       </div>
     </Fade>
